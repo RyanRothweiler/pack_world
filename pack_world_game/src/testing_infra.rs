96d4602f33591b45bf1c834a4417ac02837d0807
RyanRothweiler <ryanrothweiler@gmail.com>
2025-03-19 14:35:12 -0700

    asdf

--- pack_world_game/src/testing_infra.rs
+++ pack_world_game/src/testing_infra.rs
@@ -1,16 +1,28 @@
-use gengar_engine::{analytics::*, platform_api::*};
+use gengar_engine::{analytics::*, error::Error, platform_api::*};
 use rand::prelude::*;
 
 fn rand() -> f64 {
     rand::random_range(0.0..1.0)
 }
 
-fn send_event(vent: AnalyticsEvent) {}
+fn send_event(vent: AnalyticsEvent) {
+    unimplemented!()
+}
+
+fn write_save_game_data(data: Vec<u8>) -> Result<(), Error> {
+    unimplemented!()
+}
+
+fn get_save_game_data() -> Result<Vec<u8>, Error> {
+    unimplemented!()
+}
 
 //For testing use the windows platform api. Tests don't run on any other platform.s
 pub fn windows_plaform_api() -> PlatformApi {
     PlatformApi {
         rand: rand,
         send_event: send_event,
+        write_save_game_data: write_save_game_data,
+        get_save_game_data: get_save_game_data,
     }
 }
